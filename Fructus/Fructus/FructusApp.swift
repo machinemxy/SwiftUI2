d5df0cf1fdb8e1e97cb86d23ddbba6db777e305e
Ma Xueyuan <machinemxy@me.com>
2020-12-29 23:36:37 +0900

    set up

---
+++ Fructus/Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Ma Xueyuan on 2020/12/29.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
