76b69b128543e5dd37d6594b71fbf3c88b4d1e06
Ma Xueyuan <machinemxy@me.com>
2021-01-11 14:52:27 +0900

    setting view part 1

--- Fructus/Fructus/App/ContentView.swift
+++ Fructus/Fructus/App/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var fruits: [Fruit] = fruitsData
+    @State private var isShowingSettings: Bool = false
     
     var body: some View {
         NavigationView {
@@ -21,6 +22,16 @@ struct ContentView: View {
                 }
             } //: LIST
             .navigationTitle("Fruits")
+            .navigationBarItems(trailing:
+                Button(action: {
+                    isShowingSettings = true
+                }, label: {
+                    Image(systemName: "slider.horizontal.3")
+                }) //: BUTTON
+                .sheet(isPresented: $isShowingSettings, content: {
+                    SettingsView()
+                })
+            )
         } //: NAVIGATION
     }
 }
