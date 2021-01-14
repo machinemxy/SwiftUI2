6de4e0dad4d803161a1bad15d7b872671297028a
Ma Xueyuan <machinemxy@me.com>
2021-01-14 22:32:56 +0900

    start Africa

---
+++ Africa/Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by 馬学渊 on 2021/01/14.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
