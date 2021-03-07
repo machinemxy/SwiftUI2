f8c14a2d40518e9a6035a19fab16d5acd24ad3b0
Ma Xueyuan <machinemxy@me.com>
2021-03-07 22:26:04 +0900

    custom annotation

--- Africa/Africa/App/MapView.swift
+++ Africa/Africa/App/MapView.swift
@@ -21,15 +21,61 @@ struct MapView: View {
     
     var body: some View {
         Map(coordinateRegion: $region, annotationItems: locations) { item in
-            // MapPin(coordinate: item.location, tint: .accentColor)
-            // MapMarker(coordinate: item.location, tint: .accentColor)
+//            MapPin(coordinate: item.location, tint: .accentColor)
+//            MapMarker(coordinate: item.location, tint: .accentColor)
+//            MapAnnotation(coordinate: item.location) {
+//                Image("logo")
+//                    .resizable()
+//                    .scaledToFit()
+//                    .frame(width: 32, height: 32, alignment: .center)
+//            }
             MapAnnotation(coordinate: item.location) {
-                Image("logo")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 32, height: 32, alignment: .center)
+                MapAnnotationView(location: item)
             }
         }
+        .overlay(
+            HStack(alignment: .center, spacing: 12) {
+                Image("compass")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 48, height: 48, alignment: .center)
+                
+                VStack(alignment: .leading, spacing: 3, content: {
+                    HStack {
+                        Text("Latitude:")
+                            .font(.footnote)
+                            .fontWeight(.bold)
+                            .foregroundColor(.accentColor)
+                        Spacer()
+                        Text("\(region.center.latitude)")
+                            .font(.footnote)
+                            .foregroundColor(.white)
+                    }
+                    
+                    Divider()
+                    
+                    HStack {
+                        Text("Longitude:")
+                            .font(.footnote)
+                            .fontWeight(.bold)
+                            .foregroundColor(.accentColor)
+                        Spacer()
+                        Text("\(region.center.longitude)")
+                            .font(.footnote)
+                            .foregroundColor(.white)
+                    }
+                })
+            }
+            .padding(.vertical, 12)
+            .padding(.horizontal, 16)
+            .background(
+                Color.black
+                    .cornerRadius(8)
+                    .opacity(0.6)
+            )
+            .padding(),
+            alignment: .top
+        )
     }
 }
 
